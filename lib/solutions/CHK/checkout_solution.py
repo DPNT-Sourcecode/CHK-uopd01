3f49db81de18268c899af00e2deaa3125344b9f1
sbx_user1051 <sbx_user1051@169.254.36.113>
2022-03-21 08:57:22 +0000

    Mon Mar 21 08:57:22 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,18 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+PRICE_BY_ITEM = {"A": 50, "B": 30, "C": 20, "D": 15}
+# We'll map items to length 2 tuples such that the first element
+# is the number of items, second element is the total discounted price
+SPECIAL_OFFERS_BY_ITEM = {"A": [3, 130], "B": [2, 45]}
+
+
 def checkout(skus):
+    skus_list = skus.split(",")
+
+    # basic input validation
+    if any([sku not in PRICE_BY_ITEM for sku in skus_list]):
+        return -1
+
     raise NotImplementedError()
+
