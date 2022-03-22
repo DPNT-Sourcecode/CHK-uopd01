addcbbaef7b2d76116fd794843bad10f988eed0e
sbx_user1051 <sbx_user1051@169.254.213.189>
2022-03-22 10:05:26 +0000

    Tue Mar 22 10:05:26 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,25 +1,78 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-PRICE_BY_ITEM = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
+PRICE_BY_ITEM = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15,
+    "E": 40,
+    "F": 10,
+    "G": 20,
+    "H": 10,
+    "I": 35,
+    "J": 60,
+    "K": 70,
+    "L": 90,
+    "M": 15,
+    "N": 40,
+    "O": 10,
+    "P": 50,
+    "Q": 30,
+    "R": 50,
+    "S": 20,
+    "T": 20,
+    "U": 40,
+    "V": 50,
+    "W": 20,
+    "X": 17,
+    "Y": 20,
+    "Z": 21,
+}
 # We'll map items to lists of length 2 tuples such that the first element
 # is the number of items, second element is the total discounted price
-DISCOUNT_OFFERS_BY_ITEM = {"A": [(3, 130), (5, 200)], "B": [(2, 45)]}
+# F is a special case where it "looks" like a "get one free" offer, but it's actually a
+# discount offer since it requires at least 3 F's in the basket and applies the price of 2
+DISCOUNT_OFFERS_BY_ITEM = {
+    "A": [(3, 130), (5, 200)],
+    "B": [(2, 45)],
+    "F": [(3, 20)],
+    "H": [(5, 45), (10, 80)],
+    "K": [(2, 120)],
+    "P": [(5, 200)],
+    "Q": [(3, 80)],
+    "V": [(2, 90), (3, 130)],
+    "U": [(4, 120)],
+}
 # for get one free offers, first element is number of items, second is the item you get for free
-GET_ONE_FREE_OFFERS_BY_ITEM = {"E": (2, "B")}
+GET_ONE_FREE_OFFERS_BY_ITEM = {"E": (2, "B"), "N": (3, "M"), "R": (3, "Q")}
+# group offers, first element is the set of skus in the group, second element is the group number, third element is the price
+# I'm not sure if repeated items count for the group offer e.g. ZZX = 45 or ZZX = 2*21 + 20
+# I'll begin assuming they count because that favors the costumer
+GROUP_OFFERS = [
+    (("S", "T", "X", "Y", "Z"), 3, 45)
+]
 
 
-def compute_price_with_special_offer(*, num_items, regular_price, special_offer_quantity, special_offer_price):
+def compute_price_with_special_offers(num_items, regular_price, *offers):
+    # validate pairs of (number_of_item, offer)
+    assert all([len(offer) == 2 for offer in offers])
+
+    # sort from best to worst offer
+    offers = sorted(offers, key=lambda x: x[0], reverse=True)
+
     price = 0
-    num_special_offers = num_items // special_offer_quantity
-    num_regular_items = num_items % special_offer_quantity
-    price += num_special_offers * special_offer_price
-    price += num_regular_items * regular_price
+    remaining_items = num_items
+    # apply offers from best to worst
+    for offer_quantity, offer_price in offers:
+        num_offers = remaining_items // offer_quantity
+        remaining_items = remaining_items % offer_quantity
+        price += num_offers * offer_price
+    price += remaining_items * regular_price
     return price
 
 
 def checkout(skus):
-    print(skus)
     skus_list = [sku for sku in skus]
 
     # basic input validation
@@ -44,30 +97,26 @@ def checkout(skus):
 
     # compute total checkout value
     total = 0
+
+    # apply group offers
+    for group, necessary_quantity, offer_price in GROUP_OFFERS:
+        skus_in_group = [sku for sku in skus_list if sku in group]
+        number_of_offers = len(skus_in_group) // necessary_quantity
+        total += number_of_offers * offer_price
+        # if there are remaining items that couldn't form a group of 3
+        # we should leave them out of the offer in a way that favor the costumer
+        num_remaining_items = len(skus_in_group) % necessary_quantity
+        for i in range(num_remaining_items):
+            cheapest_sku = sorted(skus_in_group, key=lambda x: PRICE_BY_ITEM[x])[0]
+            skus_in_group.remove(cheapest_sku)
+        for sku in skus_in_group:
+            number_per_item[sku] -= 1
+
     for sku in number_per_item:
         regular_price = PRICE_BY_ITEM[sku]
-        print(number_per_item[sku], regular_price)
         if sku in DISCOUNT_OFFERS_BY_ITEM:
-            # our strategy is to apply every offer and return the highest discounted price thus finding the "best offer"
-            # however, offers can be combined to give better deals e.g. 8xA = 3XA + 5XA
-            best_discounted_price = None
-            for special_offer_quantity, special_offer_price in DISCOUNT_OFFERS_BY_ITEM[sku]:
-                discounted_price = compute_price_with_special_offer(
-                    num_items=number_per_item[sku],
-                    regular_price=regular_price,
-                    special_offer_quantity=special_offer_quantity,
-                    special_offer_price=special_offer_price,
-                )
-                if best_discounted_price is None or discounted_price < best_discounted_price:
-                    best_discounted_price = discounted_price
-
-            total += best_discounted_price
-            print(best_discounted_price)
+            total += compute_price_with_special_offers(number_per_item[sku], regular_price, *DISCOUNT_OFFERS_BY_ITEM[sku])
         else:
             total += regular_price * number_per_item[sku]
-            print(regular_price * number_per_item[sku])
 
     return total
-
-
-
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,38 +1,55 @@
 from lib.solutions.CHK.checkout_solution import checkout
-from solutions.CHK import checkout_solution
 
 
 class TestChk:
     def test_chk(self):
-        assert checkout_solution.checkout("AB") == 80
-        assert checkout_solution.checkout("AAABB") == 175
-        assert checkout_solution.checkout("AAAABBBC") == 130 + 50 + 45 + 30 + 20
+        assert checkout("AB") == 80
+        assert checkout("AAABB") == 175
+        assert checkout("AAAABBBC") == 130 + 50 + 45 + 30 + 20
+        assert checkout("") == 0
+        assert checkout("A") == 50
+        assert checkout("B") == 30
+        assert checkout("C") == 20
+        assert checkout("D") == 15
+        assert checkout("a") == -1
+        assert checkout("-") == -1
+        assert checkout("ABCa") == -1
+        assert checkout("AxA") == -1
+        assert checkout("ABCD") == 50 + 30 + 20 + 15
+        assert checkout("A") == 50
+        assert checkout("AA") == 100
+        assert checkout("ABCDABCD") == 215
+        assert checkout("BABDDCAC") == 215
+        assert checkout("ABCDCBAABCABBAAA") == 200 + 100 + 90 + 30 + 60 + 15
+        assert checkout("E") == 40
+        assert checkout("ABCDE") == 50 + 30 + 20 + 15 + 40
+        assert checkout("ABCDEE") == 50 + 30 * 0 + 20 + 15 + 2 * 40
 
-        assert checkout_solution.checkout("") == 0
-        assert checkout_solution.checkout("A") == 50
-        assert checkout_solution.checkout("B") == 30
-        assert checkout_solution.checkout("C") == 20
-        assert checkout_solution.checkout("D") == 15
-        assert checkout_solution.checkout("a") == -1
-        assert checkout_solution.checkout("-") == -1
-        assert checkout_solution.checkout("ABCa") == -1
-        assert checkout_solution.checkout("AxA") == -1
-        assert checkout_solution.checkout("ABCD") == 50 + 30 + 20 + 15
-        assert checkout_solution.checkout("A") == 50
-        assert checkout_solution.checkout("AA") == 100
-        assert checkout_solution.checkout("ABCDABCD") == 215
-        assert checkout_solution.checkout("BABDDCAC") == 215
-        assert checkout_solution.checkout("ABCDCBAABCABBAAA") == 495
+        assert checkout("AAAAA") == 200
+        assert checkout("AAAAAA") == 250
+        assert checkout("AAAAAAA") == 300
+        assert checkout("AAAAAAAA") == 330
+        assert checkout("AAAAAAAAA") == 380
+        assert checkout("AAAAAEEBAAABB") == 455
 
-        assert checkout_solution.checkout("E") == 40
-        assert checkout_solution.checkout("ABCDE") == 50 + 30 + 20 + 15 + 40
-        assert checkout_solution.checkout("ABCDEE") == 50 + 30 * 0 + 20 + 15 + 2 * 40
+        assert checkout("ABCDEF") == 50 + 30 + 20 + 15 + 40 + 10
+        assert checkout("ABCDEFF") == 50 + 30 + 20 + 15 + 40 + 20
+        assert checkout("ABCDEFFF") == 50 + 30 + 20 + 15 + 40 + 20
+        assert checkout("ABCDEFFFF") == 50 + 30 + 20 + 15 + 40 + 30
+        assert checkout("ABCDEFFFFF") == 50 + 30 + 20 + 15 + 40 + 40
+        assert checkout("ABCDEFFFFFF") == 50 + 30 + 20 + 15 + 40 + 40
 
-        assert checkout_solution.checkout("AAAAA") == 200
-        assert checkout_solution.checkout("AAAAAA") == 250
-        assert checkout_solution.checkout("AAAAAAA") == 300
-        assert checkout_solution.checkout("AAAAAAAA") == 330
-        assert checkout_solution.checkout("AAAAAAAAA") == 380
-        assert checkout_solution.checkout("AAAAAEEBAAABB") == 455
+        assert checkout("KK") == 120
 
+        assert checkout("STX") == 45
+        assert checkout("STXSTX") == 90
+        assert checkout("STXXYZ") == 90
+        assert checkout("ASYZ") == 50 + 45
+        assert checkout("STXSX") == 45 + 17 + 17
+        assert checkout("STXYZ") == 45 + 20 + 17 # TYZ + S + X benefits the costumer
+        assert checkout("XYZ") == 45
+        #assert checkout("ZZY") == 21 + 21 + 20
+        assert checkout("ZZY") == 45
+        #assert checkout("ZZSTXY") == 17 + 21 + 20 + 45# X + Z + S + ZTY
+        assert checkout("ZZSTXY") == 90
 
